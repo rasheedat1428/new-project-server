5405c094eeb83526bd2e70d79895d4b70dc06db2
Rasheedat <lateefrasheedat14@gmail.com>
2022-11-29 17:17:27 +0400

    POST and GET methods

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require("express");
+import express, { json } from "express";
+import  * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+app.use(json());
 
-const PORT = 6000;
+const PORT = process.env.PORT || 6000;
 
 const students = [
-    { name: "Habeeb", email: "habeeb@habeeb.com" },
-    { name: "Rasheedat", email: "rasheedat@habeeb.com" },
+    { id: 1, name: "Habeeb", email: "habeeb@habeeb.com" },
+    { id: 2, name: "Rasheedat", email: "rasheedat@habeeb.com" },
+    { id: 3, name: "Lateef", email: "rasheedat@habeeb.com" },
+    { id: 4, name: "Motunrayo", email: "rasheedat@habeeb.com" },
   ];
 
   app.get("/students", (req, res) => res.json(students));
+  app.get("/students/:id", (req, res) => {
+    const studentsId = req.params.id;
+    const student = students.find((s) => s.id.toString() === studentsId);
+  
+  if (student) {
+    return res.status(200).json(student)
+  } else {
+     return res.status(404).json({message: "Student not found"}) 
+  }
+  });
 
-  app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+  app.post("/students", (req, res) => {
+    const body = req.body;
+    const newStudentList = [...students, body];
+    return res.status(201).json(newStudentList);
+  });
+
+  app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
